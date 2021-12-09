50ba2d345b9462c3b842e80b78d87adf4161799e
mrinal <iammrinalkc@gmail.com>
2021-12-09 23:26:03 +0530

    callbacks

---
+++ async JS/callback.js
@@ -0,0 +1,46 @@
+
+const fs = require('fs')
+// function printFirstName(firstName , cb){
+//        console.log(firstName)
+//        cb("Sain")
+// }
+
+
+
+// function printLastName(lastName){
+//        console.log(lastName)
+// }
+
+
+// printFirstName("Mohit" , printLastName)
+
+
+//Any function that is passed as an argument to another function
+// any function that is called after a function has finished executing
+
+
+// Synchhronus way of reading file
+// console.log('Before')
+// let data = fs.readFileSync("f1.txt")
+// console.log(" "+ data)
+
+// console.log("after")
+
+
+// Asynchronus way of reading File
+// console.log("before")
+
+// fs.readFile("f1.txt" , cb)
+
+// function cb(err , data){
+//        if(err){
+//               console.log(err)
+//        }
+
+//        console.log(""+data)
+// }
+
+// console.log("after")
+
+
+
